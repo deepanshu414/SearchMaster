deb8814444d53e6dff7c8333dd9bcc6940375df1
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-07-31 18:17:53 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 setup(name="findall",
       version="1.0.0",
       description="Python script to find anything from your system.",
+      long_description=open('README.md').read(), 
+      long_description_content_type='text/markdown',
       author="Deepanshu antil",
       packages=['findall'],
       author_email="deepanshuantil4113@gmail.com",
