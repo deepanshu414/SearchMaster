f7dc671e44b772817c01b13cc4792b93e0320a53
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-07-31 14:00:44 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="find",
+      version="1.0.0",
+      description="Python script to find anything from your system.",
+      author="Deepanshu antil",
+      packages=['find'],
+      author_email="deepanshuantil4113@gmail.com",
+      url="https://github.com/deepanshu414",
+      license="MIT", 
+      install_requires=['psutil'])
