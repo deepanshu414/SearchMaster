4ac7378f274eb066938c241acd2dd26f90e40067
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-07-31 18:57:11 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
-setup(name="Filesearcher",
+setup(name="SearchMaster",
       version="1.0.0",
       description="Python script to find anything from your system.",
       long_description=open('README.md').read(), 
       long_description_content_type='text/markdown',
       author="Deepanshu antil",
-      packages=['Filesearcher'],
+      packages=['SearchMaster'],
       author_email="deepanshuantil4113@gmail.com",
       url="https://github.com/deepanshu414",
       license="MIT", 
