4250582dfbde9c6702789fc734a5a508b85ee814
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-08-03 21:19:12 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name="SearchMaster",
-      version="1.1.0",
+      version="1.1.1",
       description="Python script to find anything from your system.",
       long_description=open('README.md').read(), 
       long_description_content_type='text/markdown',
