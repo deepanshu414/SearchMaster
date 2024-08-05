9e74638f88b287f286bd34a8a08f611e7376d8f7
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-08-05 13:34:42 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 setup(name="SearchMaster",
-      version="1.1.0",
-      description="Python script to find anything from your system.",
+      version="1.1.1",
+      description="SearchMaster is a Python library that helps you search for files with a specified name across all available drives on your system.",
       long_description=open('README.md').read(), 
       long_description_content_type='text/markdown',
       author="Deepanshu antil",
+      maintainer="Deepanshu antil",
       packages=['SearchMaster'],
       author_email="deepanshuantil4113@gmail.com",
-      url="https://github.com/deepanshu414",
+      url="https://github.com/deepanshu414/find_allx",
       license="MIT", 
       install_requires=['psutil'])
