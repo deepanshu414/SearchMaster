95bca43f05969ab559972655cb8c39ae35b9d476
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-09-25 11:22:38 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name="SearchMaster",
-      version="1.1.5",
+      version="1.1.6",
       description="SearchMaster is a Python library that helps you search for files with a specified name across all available drives on your system.",
       long_description=open('README.md').read(), 
       long_description_content_type='text/markdown',
